36691361ce50c052724fafe0399bbbec788bf630
kajitanikeisuke <keisuke.kajitani@rakus-partners.co.jp>
2025-01-20 11:27:52 +0900

    やりなおし　演習

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,37 @@
 public class Human {
+private String name;
+private int age;
+private String bithplace;
+
+
+
+@Override
+public String toString() {
+    return "Human [name=" + name + ", age=" + age + ", bithplace=" + bithplace + "]";
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public int getAge() {
+    return age;
+}
+
+public void setAge(int age) {
+    this.age = age;
+}
+
+public String getBithplace() {
+    return bithplace;
+}
+
+public void setBithplace(String bithplace) {
+    this.bithplace = bithplace;
+}
 
 }
